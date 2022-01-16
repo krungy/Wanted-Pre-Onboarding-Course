220cdcf36d885ca8eed3fe71a1ece21a2d819df0
krungy <furler711@gmail.com>
2022-01-16 15:05:27 +0900

    ✨ feat : CarouselContent prop에 따른 Dim 처리 & PropTypes 추가

--- src/components/domain/CarouselContent/index.jsx
+++ src/components/domain/CarouselContent/index.jsx
@@ -1,28 +1,64 @@
 import styled from '@emotion/styled';
 import { Image, Information } from '@components/base';
+import PropTypes from 'prop-types';
 
 const CarouselContentContainer = styled.div`
   position: relative;
   padding: 0;
   margin: 0;
-  width: 1060px;
+  width: ${({ width }) => width}px;
   height: 300px;
   border-radius: 4px;
   overflow: hidden;
+  &::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    content: '';
+    background: ${({ isActive }) => !isActive && `rgba(0, 0, 0, 0.3)`};
+  }
   > div {
+    display: ${({ isActive }) => (isActive ? 'block' : 'none')};
     position: absolute;
     left: 24px;
     bottom: 28px;
   }
 `;
 
-const CarouselContent = ({ src, alt, title, subtitle, ...props }) => {
+const CarouselContent = ({
+  src,
+  alt,
+  title,
+  subtitle,
+  width,
+  isActive,
+  ...props
+}) => {
   return (
-    <CarouselContentContainer {...props}>
-      <Image src={src} alt="Image" width="100%" height="100%" />
+    <CarouselContentContainer width={width} isActive={isActive} {...props}>
+      <Image src={src} alt={alt} width="100%" height="100%" />
       <Information title={title} subtitle={subtitle}></Information>
     </CarouselContentContainer>
   );
+};
+
+CarouselContent.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  width: PropTypes.number,
+  isActive: PropTypes.bool,
+};
+
+CarouselContent.defaultProps = {
+  alt: 'CarouselImage',
+  title: 'Title',
+  subtitle: 'Subtitle',
+  width: 1060,
+  isActive: false,
 };
 
 export default CarouselContent;
