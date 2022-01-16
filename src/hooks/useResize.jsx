54ff1da726b1c2ff8e264b57ad128347469bdc75
krungy <furler711@gmail.com>
2022-01-16 21:00:49 +0900

    ✨ feat : 반응형 구현을 위한 useResize hook 추가

---
+++ src/hooks/useResize.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useResize = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowWidth;
+};
+
+export default useResize;
